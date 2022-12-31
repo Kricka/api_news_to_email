e0d31dc2e361ed629353981aaca633c6372ac10f
Milan <kricka.milan@gmail.com>
2022-12-31 21:53:31 +0100

    Gettingg news from API and sending them via email/different type of body

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ import requests
 from send_email import send_email
 
 
-url="https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d0726f52e2734be3afb0e922347416ee"
+url="https://newsapi.org/v2/top-headlines?sources=techcrunch&" \
+    "apiKey=d0726f52e2734be3afb0e922347416ee&" \
+    "language=en"
 api_key="d0726f52e2734be3afb0e922347416ee"
 
 
 request=requests.get(url)
 data=request.json()
 
+body=""
+for article in data['articles']:
+    body="Subject: Today's news" +"\n" \
+         + body + "\n" + article['title'] + "\n" + article['description']+"\n" \
+         + article['url'] +2*"\n"
 
-with open('body.txt','w') as file:
-    for article in data['articles']:
-        file.writelines(article['title']+'\n')
-        file.writelines(article['url']+'\n'+'\n')
-file.close()
-
-
-with open('body.txt','r') as file:
-    message=file.read()
-
-send_email(message)
+body=body.encode("UTF-8")
+send_email(body)
 
 

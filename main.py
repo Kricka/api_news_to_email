95bfa6a5a04648b6a5c56c3d7f583947fcde8222
Milan <kricka.milan@gmail.com>
2023-01-01 21:32:00 +0100

    Gettingg news from API about Tesla

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 from send_email import send_email
 
 
-url="https://newsapi.org/v2/top-headlines?sources=techcrunch&" \
-    "apiKey=d0726f52e2734be3afb0e922347416ee&" \
-    "language=en"
+url="https://newsapi.org/v2/everything?q=tesla&" \
+    "from=2022-12-01&sortBy=publishedAt&" \
+    "language=en&" \
+    "apiKey=d0726f52e2734be3afb0e922347416ee"
 api_key="d0726f52e2734be3afb0e922347416ee"
 
 
@@ -12,8 +13,8 @@ request=requests.get(url)
 data=request.json()
 
 body=""
-for article in data['articles']:
-    body="Subject: Today's news" +"\n" \
+for article in data['articles'][:20]:
+    body="Subject: Today's news" + "\n" \
          + body + "\n" + article['title'] + "\n" + article['description']+"\n" \
          + article['url'] +2*"\n"
 

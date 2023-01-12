daef1d74fbb9492fdc5eb13fbb6b19547af9aa79
Milan <kricka.milan@gmail.com>
2023-01-12 13:52:56 +0100

    Gettingg only positive news from API

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import requests
 from send_email import send_email
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+analyzer=SentimentIntensityAnalyzer()
 
 
-url="https://newsapi.org/v2/everything?q=tesla&" \
-    "from=2022-12-01&sortBy=publishedAt&" \
-    "language=en&" \
-    "apiKey=d0726f52e2734be3afb0e922347416ee"
-api_key="d0726f52e2734be3afb0e922347416ee"
+url="http://api.mediastack.com/v1/news?access_key=f6eaa744d81f95ec9e98d9073d4bc39a" \
+    "&countries=us,gb"
+api_key="f6eaa744d81f95ec9e98d9073d4bc39a"
 
 
 request=requests.get(url)
 data=request.json()
 
 body=""
-for article in data['articles'][:20]:
-    body="Subject: Today's news" + "\n" \
-         + body + "\n" + article['title'] + "\n" + article['description']+"\n" \
-         + article['url'] +2*"\n"
+for article in data['data']:
+    polarities=analyzer.polarity_scores(article['description'])
+    pos=polarities['pos']
+    neg = polarities['neg']
+    if pos > neg:
+        body="Subject: Today's news" + "\n" \
+             + body + "\n" + article['title'] + "\n" + article['description']+"\n" \
+             + article['url'] +2*"\n"
 
 body=body.encode("UTF-8")
 send_email(body)
